7f3d7c5c203c02624de04860ccf1f7f395215ec5
Nicolas Emery <Nicolas5150@gmail.com>
2025-05-07 08:40:37 -0700

    Env var logic change for vi in cr

--- functions/serverless/voice-connection.ts
+++ functions/serverless/voice-connection.ts
@@ -103,7 +103,9 @@ exports.handler = async function (
       debug: true, // Debugging enabled
       url: relayUrl, // Your WebSocket URL
       // @ts-expect-error - Twilio types are not up to date
-      intelligenceService: 'GAe6e37ed1b6405e90fac4dfa3dd69ab19', // Intelligence service
+      intelligenceService: isDevelopment
+        ? process.env.TWILIO_CONVERSATION_INTELLIGENCE
+        : context.TWILIO_CONVERSATION_INTELLIGENCE, // Intelligence service
     });
 
     if (segmentProfile) {
